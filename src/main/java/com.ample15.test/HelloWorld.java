47e9962be94a342058c47ad135331b273e6573ff
KAKALZF <575800503@qq.com>
2021-02-18 22:03:48 +0800

    ...

---
+++ src/main/java/com.ample15.test/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.ample15.test;
+
+import com.kuaidi100.utils.HttpRequest;
+
+/**
+ * @author zefeng_lin
+ * @date 2021-02-18 16:39
+ * @description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        HttpRequest httpRequest = new HttpRequest();
+        System.out.println(httpRequest);
+    }
+}
---
+++ src/main/java/com.ample15.test/HelloWorld2.java
@@ -0,0 +1,16 @@
+package com.ample15.test;
+
+import com.kuaidi100.utils.HttpRequest;
+
+/**
+ * @author zefeng_lin
+ * @date 2021-02-18 16:39
+ * @description
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World2");
+        HttpRequest httpRequest = new HttpRequest();
+        System.out.println(httpRequest);
+    }
+}
